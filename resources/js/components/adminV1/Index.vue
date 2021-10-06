1a13ce3287b89f91de2d11ee4c387c8e09c7cd41
Hilmi <muhammadhilmi92101@gmail.com>
2021-10-06 19:43:11 +0800

    Add recent order grid

--- resources/js/components/adminV1/Index.vue
+++ resources/js/components/adminV1/Index.vue
@@ -16,7 +16,7 @@
                     </ul>
                 </div>
 
-                <div class="main">
+                <div :class="{active: sidebarIsActive}" class="main">
                     <div class="topbar">
                         <div @click="toggleSidebar()" class="toggle">
                             <ion-icon name="menu-outline"></ion-icon>
@@ -29,6 +29,50 @@
                         </div>
                         <div class="user">
                             <img :src="images.user" alt="">
+                        </div>
+                    </div>
+
+                    <div class="card-box">
+                        <div v-for="(card,index) in cards" class="custom-card-1">
+                            <div>
+                                <div class="numbers">{{ card.number }}</div>
+                                <div class="card-name">{{ card.name }}</div>
+                            </div>
+                            <div class="icon-box">
+                                <ion-icon :name="card.icon"></ion-icon>
+                            </div>
+                        </div> 
+                    </div>
+                    
+                    <div class="details">
+                        <div class="recent-orders">
+                            <div class="custom-card-header">
+                                <h2>Recent Orders</h2>
+                                <a href="#" class="custom-button-1">View All</a>
+                            </div>
+
+                            <table class="custom-table-1">
+                                <thead>
+                                    <tr>
+                                        <td>Name</td>
+                                        <td>Price</td>
+                                        <td>Payment</td>
+                                        <td>Status</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="(order,index) in orders">
+                                        <td>{{ order.name }}</td>
+                                        <td>{{ order.price }}</td>
+                                        <td>{{ order.payment }}</td>
+                                        <td>
+                                            <span class="status delivered">
+                                                {{ order.status }}
+                                            </span>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
@@ -48,7 +92,7 @@
     	data() {   
             return {   
 
-                sidebarIsActive: true,
+                sidebarIsActive: false,
                 links: [
 
                     {
@@ -93,6 +137,88 @@
                     },
                     
                     
+                ],
+
+                cards: [
+                    {
+                        name: 'Daily Views',
+                        number: '1, 503',
+                        icon: 'eye-outline',
+                    },
+                    {
+                        name: 'Sales',
+                        number: '80',
+                        icon: 'cart-outline',
+                    },
+                    {
+                        name: 'Comments',
+                        number: '284',
+                        icon: 'chatbubbles-outline',
+                    },
+                    {
+                        name: 'Earning',
+                        number: '$7, 842',
+                        icon: 'cash-outline',
+                    },
+
+                ],
+
+                orders: [
+
+                    {
+                        name: 'PS4',
+                        price: '$1, 500',
+                        payment: 'Paid',
+                        status: 'Delivered',
+                    },
+                    {
+                        name: 'PS5',
+                        price: '$2, 500',
+                        payment: 'Paid',
+                        status: 'Delivered',
+                    },
+                    {
+                        name: 'PS3',
+                        price: '$1, 000',
+                        payment: 'Paid',
+                        status: 'Delivered',
+                    },
+                    {
+                        name: 'PS2',
+                        price: '$500',
+                        payment: 'Paid',
+                        status: 'Delivered',
+                    },
+                    {
+                        name: 'PS2',
+                        price: '$500',
+                        payment: 'Paid',
+                        status: 'Delivered',
+                    },
+                    {
+                        name: 'PS2',
+                        price: '$500',
+                        payment: 'Paid',
+                        status: 'Delivered',
+                    },
+                    {
+                        name: 'PS2',
+                        price: '$500',
+                        payment: 'Paid',
+                        status: 'Delivered',
+                    },
+                    {
+                        name: 'PS2',
+                        price: '$500',
+                        payment: 'Paid',
+                        status: 'Delivered',
+                    },
+                    {
+                        name: 'PS2',
+                        price: '$500',
+                        payment: 'Paid',
+                        status: 'Delivered',
+                    },
                 ],
 
                 images: {
